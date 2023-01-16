fdfb22aec0027e552ee3d19615be0eac70927b96
Vidhi Makani <100896986+vidhimakani74@users.noreply.github.com>
2023-01-16 17:48:48 +0530

    initial commits

---
+++ script.js
@@ -0,0 +1,31 @@
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn = "X"
+let isgameover=false;
+
+//function for change turn 
+function changeTurn(){
+    return turn === "X"?"0":"X"
+}
+
+//function forn check win
+function checkWin(){
+    let boxtext=document.getElementsByClassName("boxtext");
+    let wins=[
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
+    ]
+    wins.forEach(e=>{
+
+    })
+}
+
+//game logic
+let boxes=document.getElementsByClassName("box");
